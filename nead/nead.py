218e6894c715bc00ca4b818298a188ede8fa0d7d
BaptisteVandecrux <b.vandecrux@gmail.com>
2020-12-08 16:40:39 +0100

    small fixes

--- nead/nead.py
+++ nead/nead.py
@@ -120,11 +120,23 @@ def read(neadfile, MKS=None, multi_index=True, index_col=None):
 
     return ds
 
-#%%
+
+
+#%% Writes NEAD file (CSV file with NEAD formatted header)
+# Columns written in NEAD output will be the fields designated in the
+#   NEAD configuration file setting 'fields' in section 'FIELDS'
+# Paths must be absolute
+# Paths can use 'r' in front of strings with paths to designate them as a raw string if using Windows
+#   style paths with backslashes
+# Arguments:
+#   data_frame    Pandas dataframe
+#   nead_config   string with Path to NEAD configuration file, ex. r'C:\Users\Icy\gcnet\config\nead_header.ini'
+#   output_path   string with Path assigned to output NEAD file,
+#                 be sure to designate name for new NEAD file at end of string
+#                 ex.  r'C:\Icy\gcnet\output\nead\my_nead_file_name')
 from io import StringIO
 import configparser
 from pathlib import Path
-
 
 def read_config(config_path: str):
     config_file = Path(config_path)
@@ -146,19 +158,6 @@ def get_hashed_lines(config):
         hash_lines.append(line)
     return hash_lines
 
-
-#%% Writes NEAD file (CSV file with NEAD formatted header)
-# Columns written in NEAD output will be the fields designated in the
-#   NEAD configuration file setting 'fields' in section 'FIELDS'
-# Paths must be absolute
-# Paths can use 'r' in front of strings with paths to designate them as a raw string if using Windows
-#   style paths with backslashes
-# Arguments:
-#   data_frame    Pandas dataframe
-#   nead_config   string with Path to NEAD configuration file, ex. r'C:\Users\Icy\gcnet\config\nead_header.ini'
-#   output_path   string with Path assigned to output NEAD file,
-#                 be sure to designate name for new NEAD file at end of string
-#                 ex.  r'C:\Icy\gcnet\output\nead\my_nead_file_name')
 def write(data_frame, nead_config, output_path):
     # Assign nead_output to output_path with .csv extension
     nead_output = Path('{0}'.format(output_path))
@@ -179,6 +178,7 @@ def write(data_frame, nead_config, output_path):
 
     # Write hash_lines into nead_header
     with open(nead_output, 'w', newline='\n') as nead_header:
+        nead_header.write('# NEAD 1.0 UTF-8\n')
         for row in hash_lines:
             nead_header.write(row)
 
--- nead/tests/test_nead.py
+++ nead/tests/test_nead.py
@@ -25,9 +25,9 @@ def test_write():
     ds = nead.read(fname, index_col=0, MKS=True)
     df = ds.to_dataframe()
     df = df.reset_index()
-    nead.write(df, nead_config = 'sample_header.ini', output_path = 'sample_out2.csv')
+    nead.write(df, nead_config = 'sample_header.ini', output_path = 'sample_out.csv')
 
-    ds2 = nead.read(fname, index_col=0, MKS=True)
+    ds2 = nead.read('sample_out.csv', index_col=0, MKS=True)
     print(ds2)
 
     
