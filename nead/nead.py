8ad36e0af0b610a765d70e3531f76e7591bbb26a
BaptisteVandecrux <b.vandecrux@gmail.com>
2023-01-13 12:50:05 +0100

    Update nead.py

--- nead/nead.py
+++ nead/nead.py
@@ -62,9 +62,9 @@ def read(neadfile, MKS=None, multi_index=True, index_col=None):
 
             # Convert from string to number if it is a number
             if val.strip('-').strip('+').replace('.','').isdigit():
-                val = np.float(val)
-                if val == np.int(val):
-                    val = np.int(val)
+                val = float(val)
+                if val == int(val):
+                    val = int(val)
 
             if section == 'meta': meta[key] = val
             if section == 'fields': fields[key] = val
